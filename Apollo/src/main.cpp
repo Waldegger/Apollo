e631b6eb9355e2754b2f1e640305389847d31602
Waldegger <Stefan.Waldegger@users.noreply.github.com>
2022-11-12 22:27:04 +0100

    Window is opened

--- Apollo/src/main.cpp
+++ Apollo/src/main.cpp
@@ -1,6 +1,60 @@
 #include <iostream>
 
+#include <SDL2/SDL.h>
+
 int main(int argc, char* argv[])
 {
+	static constexpr int SCREEN_WIDTH = 640;
+	static constexpr int SCREEN_HEIGHT = 480;
 
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        std::cout << "Failed to initialize the SDL2 library\n";
+        std::cout << "SDL2 Error: " << SDL_GetError() << "\n";
+        return -1;
+    }
+
+    SDL_Window* window = SDL_CreateWindow("SDL2 Window",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        SCREEN_WIDTH, SCREEN_HEIGHT,
+        0);
+
+    if (!window)
+    {
+        std::cout << "Failed to create window\n";
+        std::cout << "SDL2 Error: " << SDL_GetError() << "\n";
+        return -1;
+    }
+
+    SDL_Surface* window_surface = SDL_GetWindowSurface(window);
+
+    if (!window_surface)
+    {
+        std::cout << "Failed to get the surface from the window\n";
+        std::cout << "SDL2 Error: " << SDL_GetError() << "\n";
+        return -1;
+    }
+
+    bool keep_window_open = true;
+    while (keep_window_open)
+    {
+        SDL_Event e;
+        while (SDL_PollEvent(&e) > 0)
+        {
+            switch (e.type)
+            {
+            case SDL_QUIT:
+                keep_window_open = false;
+                break;
+            }
+
+            SDL_UpdateWindowSurface(window);
+        }
+    }
+
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+
+	return EXIT_SUCCESS;
 }
---
+++ Apollo/src/window.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <SDL2/SDL.h>
+
+namespace apollo
+{
+	class window
+	{
+	public:
+		window();
+	public:
+
+	protected:
+
+	private:
+	};
+}
