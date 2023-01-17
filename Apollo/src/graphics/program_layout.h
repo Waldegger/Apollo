95565d00fbc4d3387a438e92fc5aba182a1180d2
Waldegger <Stefan.Waldegger@users.noreply.github.com>
2023-01-17 19:16:06 +0100

    programlayour can now take strings of the uniforms in consturctor

--- Apollo/src/graphics/program_layout.h
+++ Apollo/src/graphics/program_layout.h
@@ -19,6 +19,12 @@ namespace age
 			, m_mvp_matrix_location{ mvp_matrix_location }
 			, m_texture_location{ texture_location }
 		{}
+
+		program_layout(const shader_program& program, std::string_view mvp_matrix_name, std::string_view texture_name)
+			: m_program{ program }
+			, m_mvp_matrix_location{ m_program.get().get_uniform_location(mvp_matrix_name) }
+			, m_texture_location{ m_program.get().get_uniform_location(texture_name) }
+		{}
 		
 	public:
 		inline const shader_program& get_program() const { return m_program; }
@@ -29,8 +35,8 @@ namespace age
 		inline void set_texture_location(int32_t value) { m_texture_location = value; }
 		inline int32_t get_texture_location() const { return m_texture_location; }
 
-		inline void mvp_matrix_name(const std::string_view& name) { m_mvp_matrix_location = m_program.get().get_uniform_location(name); }
-		inline void texture_name(const std::string_view& name) { m_texture_location = m_program.get().get_uniform_location(name); }
+		inline void mvp_matrix_name(const std::string_view name) { m_mvp_matrix_location = m_program.get().get_uniform_location(name); }
+		inline void texture_name(const std::string_view name) { m_texture_location = m_program.get().get_uniform_location(name); }
 	protected:
 
 	private:
